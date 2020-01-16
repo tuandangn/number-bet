8b883534862918beb7e116ce6bc6664cf7268603
Tuan Dang <nguyentuandang7@gmail.com>
2020-01-16 22:28:50 +0700

    Update Project Dependencies

--- Crawler/NB.Crawler.Services.Tests/NbCrawlerServiceTests.cs
+++ Crawler/NB.Crawler.Services.Tests/NbCrawlerServiceTests.cs
@@ -1,5 +1,9 @@
-﻿using NB.Crawler.Core.Domain;
+﻿using System;
+using Moq;
+using MrdLibraries.Data;
+using NB.Crawler.Core.Domain;
 using Xunit;
+using MrdLibraries.TestUtilities;
 
 namespace NB.Crawler.Services.Tests
 {
@@ -8,7 +12,19 @@ namespace NB.Crawler.Services.Tests
         [Fact]
         public async void HasDataAsync()
         {
-            //NumberSource[] availableNumberSources = new[] {new NumberSource { CreatedOnUtc} }
+            var checkDate = DateTime.UtcNow;
+            NumberSource[] availableNumberSources = new[] {
+                new NumberSource { CreatedOnUtc = DateTime.UtcNow}
+            };
+            var numberSourceRepositoryMock = new Mock<IRepository<NumberSource>>();
+            numberSourceRepositoryMock.Setup(r => r.Table).Returns(availableNumberSources.ToAsync())
+                .Verifiable();
+            var nbCrawlerService = new NbCrawlerService(numberSourceRepositoryMock.Object);
+
+            var result = await nbCrawlerService.HasDataAsync(checkDate);
+
+            Assert.True(result);
+            numberSourceRepositoryMock.Verify();
         }
     }
 }
--- Crawler/NB.Crawler.Services/NbCrawlerService.cs
+++ Crawler/NB.Crawler.Services/NbCrawlerService.cs
@@ -1,6 +1,8 @@
-﻿using MrdLibraries.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MrdLibraries.Data;
 using NB.Crawler.Core.Domain;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NB.Crawler.Services
@@ -19,9 +21,13 @@ namespace NB.Crawler.Services
             throw new NotImplementedException();
         }
 
-        public Task<bool> HasDataAsync(DateTime date)
+        public async Task<bool> HasDataAsync(DateTime date)
         {
-            throw new NotImplementedException();
+            var query = from numberSource in _numberSourceRepository.Table
+                where numberSource.CreatedOnUtc == date
+                select numberSource;
+            var result = await query.AnyAsync();
+            return result;
         }
     }
 }
